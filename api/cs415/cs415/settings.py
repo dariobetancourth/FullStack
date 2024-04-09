b124d1f4e75ac776593a7ce1057a76a026db431d
dariobetancourth <156965416+dariobetancourth@users.noreply.github.com>
2024-04-08 18:46:47 -0600

    endpoints

--- api/cs415/cs415/settings.py
+++ api/cs415/cs415/settings.py
@@ -93,6 +93,9 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+# Turn on/off jwt Auth Header validation
+JWT_AUTH = False
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
